d7ce3a699f7200288f8c1007f4b1f0399de5baad
Muges <git@muges.fr>
2017-09-08 23:20:59 +0200

    add examples/sin.py to demonstrate ArrayReader/Writer

---
+++ examples/sine.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+sine
+~~~~
+
+Run a TSM procedure on a signal generated with numpy.
+"""
+# pylint: disable=invalid-name
+
+import numpy as np
+import sounddevice as sd
+from audiotsm.ola import ola
+from audiotsm.io.array import ArrayReader, ArrayWriter
+
+
+# The parameters of the input signal
+length = 1  # in seconds
+samplerate = 44100  # in Hz
+frequency = 440  # an A4
+
+# Generate the input signal
+time = np.linspace(0, length, int(length * samplerate))
+input_signal = np.sin(np.pi * frequency * time).reshape((1, -1))
+
+# Run the TSM procedure
+reader = ArrayReader(input_signal)
+writer = ArrayWriter(channels=1)
+
+tsm = ola(channels=1, speed=0.5)
+
+finished = False
+while not (finished and reader.empty):
+    tsm.read_from(reader)
+    _, finished = tsm.write_to(writer)
+
+finished = False
+while not finished:
+    _, finished = tsm.flush_to(writer)
+
+# Play the output
+# This example was written to show how to use an ArrayWriter. If you want to
+# play the output of a TSM procedure you should use an
+# audiotsm.io.sounddevice.StreamWriter.
+sd.play(np.ascontiguousarray(writer.data.T), samplerate, blocking=True)
