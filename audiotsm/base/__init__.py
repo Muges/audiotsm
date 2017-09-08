0f44b9f819e32c40d10cceedefea5b5d6174a763
Muges <git@muges.fr>
2017-09-08 16:02:21 +0200

    Add abstract class for TSM procedures

---
+++ audiotsm/base/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+audiotsm.base
+~~~~~~~~~~~~~
+
+This module provides base classes for the implementation of time-scale
+modification procedures.
+"""
+
+from .tsm import TSM  # noqa: F401
+from .analysis_synthesis import AnalysisSynthesisTSM, Converter  # noqa: F401
