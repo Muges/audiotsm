ac03d170931a022b52bbd620d738592356c968e4
Muges <git@muges.fr>
2017-09-14 00:14:50 +0200

    add GstTSM class

---
+++ audiotsm/gstreamer/base.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+
+"""
+The :mod:`~audiotsm.gstreamer.base` module provides a base class for gstreamer
+plugin using :class:`~audiotsm.base.tsm.TSM` objects.
+"""
+
+import sys
+import numpy as np
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version('GstAudio', '1.0')
+
+# pylint: disable=wrong-import-position
+from gi.repository import GObject, GLib, Gst, GstAudio
+from audiotsm import __version__
+from audiotsm.io.array import ArrayReader, ArrayWriter
+# pylint: enable=wrong-import-position
+
+Gst.init(sys.argv)
+
+
+def audioformatinfo_to_dtype(info):
+    """Return the data type corresponding to a
+    :class:`GstAudio.AudioFormatInfo` object.
+
+    :param info: a :class:`GstAudio.AudioFormatInfo`.
+    :returns: the corresponding data type, to be used in :mod:`numpy`
+    functions.
+    """
+    endianness = '<' if info.endianness == GLib.LITTLE_ENDIAN else '>'
+
+    if info.flags & GstAudio.AudioFormatFlags.INTEGER:
+        if info.flags & GstAudio.AudioFormatFlags.SIGNED:
+            _type = 'i'
+        else:
+            _type = 'u'
+    elif info.flags & GstAudio.AudioFormatFlags.FLOAT:
+        _type = 'f'
+    else:
+        raise ValueError(
+            'unsupported audio format flags: {}'.format(info.flags))
+
+    samplewidth = info.width // 8  # in bytes
+
+    return '{}{}{}'.format(endianness, _type, samplewidth)
+
+
+class GstTSM(GstAudio.AudioFilter):
+    """Gstreamer TSM plugin.
+
+    Subclasses should implement the :func:`~GstTSM.create_tsm` method and
+    provide two class attributes:
+
+    - ``__gstmetadata__ = (longname, classification, description, author)``.
+      See the documentation of the gst_element_class_set_metadata_ function for
+      more details.
+    - ``plugin_name``, the name of the plugin.
+
+    Calling the :func:`~GstTSM.register` class method on a subclass will
+    register it, enabling you to instantiate an audio filter with
+    ``Gst.ElementFactory.make(plugin_name)``.
+
+    .. _gst_element_class_set_metadata:
+        https://gstreamer.freedesktop.org/data/doc/gstreamer/head/gstreamer/html/GstElement.html#gst-element-class-set-metadata
+    """  # noqa: E501
+
+    __gsttemplates__ = (Gst.PadTemplate.new("src",
+                                            Gst.PadDirection.SRC,
+                                            Gst.PadPresence.ALWAYS,
+                                            Gst.Caps.new_any()),
+                        Gst.PadTemplate.new("sink",
+                                            Gst.PadDirection.SINK,
+                                            Gst.PadPresence.ALWAYS,
+                                            Gst.Caps.new_any()))
+
+    @classmethod
+    def plugin_init(cls, plugin):
+        """Initialize the plugin."""
+        plugin_type = GObject.type_register(cls)
+        Gst.Element.register(plugin, cls.plugin_name, 0, plugin_type)
+        return True
+
+    @classmethod
+    def register(cls):
+        """Register the plugin.
+
+        Register the plugin to make it possible to instantiate it with
+        :func:`Gst.ElementFactory.make."""
+        Gst.Plugin.register_static(
+            Gst.VERSION_MAJOR, Gst.VERSION_MINOR, cls.plugin_name,
+            cls.get_metadata('description'), cls.plugin_init, __version__,
+            'MIT/X11', 'audiotsm', 'audiotsm', '')
+
+    def do_setup(self, audioinfo):
+        """Setup the audio filter.
+
+        This method is called once when the playback starts."""
+        if audioinfo.layout != GstAudio.AudioLayout.INTERLEAVED:
+            raise TypeError(
+                "audiotsm.gstreamer only supports interleaved audio.")
+
+        # pylint: disable=attribute-defined-outside-init
+        self._audioinfo = audioinfo
+        self._channels = audioinfo.channels
+        self._samplerate = audioinfo.rate
+        self._dtype = audioformatinfo_to_dtype(audioinfo.finfo)
+
+        self._tsm = self.create_tsm(audioinfo.channels, speed=1.5)
+        self._position = 0
+        return True
+
+    def _gstbuffer_to_ndarray(self, gstbuffer):
+        """Return the data contained in ``gstbuffer`` as a
+        :class:`numpy.ndarray`.
+
+        :param gstbuffer: a :class:`Gst.Buffer`.
+        """
+        _, mapinfo = gstbuffer.map(Gst.MapFlags.READ)
+        data = mapinfo.data
+        gstbuffer.unmap(mapinfo)
+
+        data = np.frombuffer(data, self._dtype).astype(np.float32) / 32676
+        data = data.reshape((-1, self._channels)).T
+
+        return data
+
+    def _ndarray_to_gstbuffer(self, gstbuffer, data):
+        """Write the ``data`` to ``gstbuffer``.
+
+        This method is a bit hack-ish. It would be better to set the size of
+        the :class:`Gst.Buffer` in advance with the
+        :func:`GstBase.BaseTransform.do_transform_size` virtual method, but it
+        is not possible to do so with pygst.
+
+        :param gstbuffer: a :class:`Gst.Buffer`.
+        :param data: a :class:`numpy.ndarray`.
+        """
+        length = data.shape[1]
+        data = (data.T.reshape((-1,)) * 32676).astype(self._dtype).tobytes()
+        size = len(data)
+
+        # Copy as many bytes as possible to the buffer directly
+        n = min(len(data), gstbuffer.get_size())
+        if n > 0:
+            gstbuffer.fill(0, data[:n])
+            data = data[n:]
+
+        # Allocate memory for the rest
+        mem = Gst.Memory.new_wrapped(0, data, len(data), 0, None, None)
+        gstbuffer.append_memory(mem)
+        gstbuffer.set_size(size)
+
+        duration = (length * 1000000000) // self._samplerate
+        gstbuffer.duration = duration
+
+    def do_transform(self, in_buffer, out_buffer):
+        """Run the data of ``in_buffer`` through the
+        :class:`~audiotsm.base.tsm.TSM` object and write the output to
+        ``out_buffer``.
+
+        :param in_buffer: a :class:`Gst.Buffer` containing the input data.
+        :param out_buffer: a :class:`Gst.Buffer` where the output data will be
+            written.
+        """
+        # There is a bug that increases the refcount of out_buffer, making it
+        # non writable (https://bugzilla.gnome.org/show_bug.cgi?id=727702#c4).
+        # Set the refcount to 1 to fix this.
+        refcount = out_buffer.mini_object.refcount
+        out_buffer.mini_object.refcount = 1
+
+        # Set the position of the output buffer
+        out_buffer.pts = self._position
+
+        # Run the TSM procedure
+        reader = ArrayReader(self._gstbuffer_to_ndarray(in_buffer))
+        writer = ArrayWriter(self._channels)
+
+        self._tsm.run(reader, writer, flush=False)
+
+        self._ndarray_to_gstbuffer(out_buffer, writer.data)
+        self._position += out_buffer.duration
+
+        # Reset the refcount
+        out_buffer.mini_object.refcount = refcount
+
+        return Gst.FlowReturn.OK
+
+    def create_tsm(self, channels, speed):
+        """Returns the :class:`~audiotsm.base.tsm.TSM` object used by the audio
+        filter."""
+        raise NotImplementedError()
